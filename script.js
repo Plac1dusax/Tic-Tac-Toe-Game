f415a6a5cc508da3985aa87bc0ce3488df0164df
Plac1dusax <FSDeryol@gmail.com>
2022-10-02 22:32:41 +0300

    functions for game result and the  restart button added

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const X = "turn-x"
-const O = "turn-o"
 const cells = document.querySelectorAll(".cell")
 const gameTable = document.querySelector(".game-table")
 const winningConditions = [
@@ -13,20 +11,44 @@ const winningConditions = [
   [2, 4, 6]
 ]
 const gameResultScreen = document.querySelector(".game-result-screen")
+const gameResultText = document.querySelector(".game-result-text")
 const restartButton = gameResultScreen.querySelector("button")
 let turn = false
 let currentTurn = "x"
 
 gameTable.classList.add("turn-x")
 
-cells.forEach(cell => {
-  cell.addEventListener("click", handleTurn, { once: true })
+restartButton.addEventListener("click", () => {
+  cells.forEach(cell => {
+    cell.classList.remove("x")
+    cell.classList.remove("o")
+  })
+  gameTable.classList.remove("turn-x")
+  gameTable.classList.remove("turn-o")
+
+  gameTable.classList.add("turn-x")
+  turn = false
+  currentTurn = "x"
+
+  gameResultScreen.style.display = "none"
 })
+
+function handleClick(cells) {
+  cells.forEach(cell => {
+    cell.addEventListener("click", handleTurn, { once: true })
+  })
+}
+
+handleClick(cells)
 
 function handleTurn(e) {
   turn ? e.target.classList.add("o") : e.target.classList.add("x")
-  if (drawGame()) return drawGame()
-  if (checkGame(currentTurn)) return restartGame()
+
+  if (checkGame(currentTurn)) {
+    return handleWin(currentTurn)
+  }
+
+  if (drawGame()) return handleDraw()
 
   if (turn === false) {
     turn = true
@@ -47,4 +69,30 @@ function checkGame(currentTurn) {
       if (cells[index].classList.contains(currentTurn)) return true
     })
   })
+}
+
+function drawGame() {
+  let sum = 0
+
+  for (let i = 0; i < cells.length; i++) {
+    if (cells[i].classList.contains("x") || cells[i].classList.contains("o")) {
+      sum++
+    }
+  }
+
+  if (sum === 9) return true
+}
+
+function handleWin(currentTurn) {
+  gameResultText.textContent = `Winner: ${currentTurn.toUpperCase()}!`
+  gameResultScreen.style.display = "flex"
+
+  handleClick(cells)
+}
+
+function handleDraw() {
+  gameResultText.textContent = "Draw !"
+  gameResultScreen.style.display = "flex"
+
+  handleClick(cells)
 }
